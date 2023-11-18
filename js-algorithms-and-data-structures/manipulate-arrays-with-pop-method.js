f36a5db43ca10ee01916f641bc62cea4947dd02e
SaschukV <v.saschuk@gmail.com>
2023-11-19 00:22:00 +0200

    Manipulate Arrays With pop Method

---
+++ js-algorithms-and-data-structures/manipulate-arrays-with-pop-method.js
@@ -0,0 +1,11 @@
+// ❓DESCRIPTION:
+
+// ✅ Solution1:(моё решение)
+const myArray = [
+  ['John', 23],
+  ['cat', 2],
+]
+const removedFromMyArray = myArray.pop()
+
+console.log(myArray) // [['John,23]]
+console.log(removedFromMyArray) // ['cat',2]
