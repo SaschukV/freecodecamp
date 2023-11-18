7ea5a6ee663b8fba3b0f8dbd36f5380d793e154d
SaschukV <v.saschuk@gmail.com>
2023-11-18 22:09:18 +0200

    find the second-to-last symbol in the string.

---
+++ js-algorithms-and-data-structures/find-second-to-last-symbol-in the-string.js
@@ -0,0 +1,8 @@
+// ❓DESCRIPTION:
+// Use bracket notation to find the second-to-last character in the lastName string.
+
+// ✅ Solution1:(моё решение)
+const lastName = 'Lovelace'
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]
+
+console.log(lastName[lastName.length - 2]) // 'c'
