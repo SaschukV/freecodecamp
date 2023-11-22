be3aa8229f61769e702541406b3a2a01c0869f93
SaschukV <v.saschuk@gmail.com>
2023-11-22 23:22:59 +0200

    Replace Loops using Recursion

---
+++ js-algorithms-and-data-structures/replace-loops-using-recursion.js
@@ -0,0 +1,22 @@
+// ❓DESCRIPTION:
+
+// ✅ Solution1:(моё решение)
+
+function multiply1(arr, n) {
+  if (n <= 0) {
+    return 1
+  } else {
+    return multiply1(arr, n - 1) * arr[n - 1]
+  }
+}
+console.log(multiply1([2, 2, 3], 3)) // 12
+
+// ✅ Solution2:(моё решение)
+function multiply2(arr, n) {
+  let product = 1
+  for (let i = 0; i < n; i++) {
+    product *= arr[i]
+  }
+  return product
+}
+console.log(multiply2([2, 2, 3], 3)) // 12
