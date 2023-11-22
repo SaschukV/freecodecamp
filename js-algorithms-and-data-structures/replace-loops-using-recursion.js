cad4826d51a3585e3faece5aa752e8ee022eb5b1
SaschukV <v.saschuk@gmail.com>
2023-11-22 23:35:51 +0200

    Replace Loops using Recursion (add function sum())

--- js-algorithms-and-data-structures/replace-loops-using-recursion.js
+++ js-algorithms-and-data-structures/replace-loops-using-recursion.js
@@ -22,3 +22,28 @@ function multiply2(arr, n) {
   return product
 }
 console.log(multiply2([2, 2, 3], 3)) // 12
+
+// ❓DESCRIPTION:
+// Write a recursive function,sum(arr, n),
+// that returns the sum of the first n elements of an array arr.
+
+// ✅ Solution1:(моё решение)
+function sum1(arr, n) {
+  if (n <= 0) {
+    return 0
+  } else {
+    return sum1(arr, n - 1) + arr[n - 1]
+  }
+}
+console.log(sum1([2, 2, 3], 3)) // 7
+
+// ✅ Solution2:(моё решение)
+
+function sum2(arr, n) {
+  let product = 0
+  for (let i = 0; i < n; i++) {
+    product += arr[i]
+  }
+  return product
+}
+console.log(sum2([2, 2, 3], 3)) // 7
