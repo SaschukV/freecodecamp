8443cd2ce0db9f6f639ff2834fb29ddb399c2a49
SaschukV <v.saschuk@gmail.com>
2023-11-24 22:51:02 +0200

    generate random from 0 to 9

---
+++ js-algorithms-and-data-structures/generate-random-from-0-to-9.js
@@ -0,0 +1,8 @@
+// ❓DESCRIPTION:
+// To generate and return a random whole number in the range from 0 to 9.
+
+// ✅ Solution1:(моё решение)
+function randomWholeNum() {
+  return Math.floor(Math.random() * 10)
+}
+console.log(randomWholeNum()) // 4,6,8 ........
