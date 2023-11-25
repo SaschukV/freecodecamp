a5380bae8f037fee89e8bb1ccc4e1a08cd929bbb
SaschukV <v.saschuk@gmail.com>
2023-11-25 13:04:04 +0200

    convert to integer

---
+++ js-algorithms-and-data-structures/convert-to-integer.js
@@ -0,0 +1,11 @@
+// ❓DESCRIPTION:
+// Use parseInt() in the convertToInteger function so it converts the input
+// string str into an integer, and returns it.
+
+// ✅ Solution1:(моё решение)
+
+function convertToInteger(str) {
+  return parseInt(str)
+}
+
+console.log(convertToInteger('00056'))
